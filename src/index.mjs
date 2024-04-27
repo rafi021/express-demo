92cfc52af21fc85db9f3bb29142da2ebfff1d709
rafi021 <mahmud.ibrahim021@gmail.com>
2024-04-27 15:43:18 +0600

    using express middleware

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,15 @@
 import express from "express";
 import { PORT } from "./config/env.js";
 import router from "./routes/index.js";
+import logger from "./middleware/logger.js";
 
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public')); 
+app.use(logger);
 
 // register routes
 app.use(router);
