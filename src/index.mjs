1db148767e1521ff1620870977941dc94e262e0d
rafi021 <mahmud.ibrahim021@gmail.com>
2024-04-27 15:49:18 +0600

    3rd party usefull middleware

--- src/index.mjs
+++ src/index.mjs
@@ -2,13 +2,20 @@ import express from "express";
 import { PORT } from "./config/env.js";
 import router from "./routes/index.js";
 import logger from "./middleware/logger.js";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public')); 
+app.use(express.static("public"));
+app.use(helmet());
+app.use(cors());
+app.use(morgan("tiny"));
+
 app.use(logger);
 
 // register routes
